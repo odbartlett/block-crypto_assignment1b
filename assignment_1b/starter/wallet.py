a6cd83cb1ec67bc91efb1a1da5233fd9699d0da0
Owen Bartlett <owenbartlett1@gmail.com>
2026-02-22 13:55:46 -0500

    Wallet not implemented yet

--- assignment_1b/starter/wallet.py
+++ assignment_1b/starter/wallet.py
@@ -32,36 +32,6 @@ def build_transaction(inputs: List[Input], outputs: List[Output], signing_key: S
     5. Create scriptSig for each input with signature and public key
     6. Return the signed transaction
     """
+    # TODO: Implement build_transaction
     # Hint: Use Script.p2pkh_unlocking_script(signature, pub_key) for scriptSig
-    if not inputs or not outputs:
-        return None
-
-    pub_key = signing_key.verify_key.encode().hex()
-    pub_key_hash = sha256_hash(signing_key.verify_key.encode()).hex()
-
-    input_sum = 0
-    seen_txids = set()
-    for inp in inputs:
-        if inp.tx_hash in seen_txids:
-            return None
-        seen_txids.add(inp.tx_hash)
-        # Check that we can spend this input (pubkey hash matches)
-        expected_hash = inp.output.script_pubkey.elements[2]
-        if expected_hash != pub_key_hash:
-            return None
-        input_sum += inp.output.value
-
-    output_sum = sum(o.value for o in outputs)
-    if input_sum != output_sum:
-        return None
-
-    unsigned_inputs = [Input(inp.output, inp.tx_hash, Script([])) for inp in inputs]
-    unsigned_tx = Transaction(unsigned_inputs, outputs)
-    tx_data = bytes.fromhex(unsigned_tx.bytes_to_sign())
-    signature = signing_key.sign(tx_data).signature.hex()
-
-    signed_inputs = []
-    for inp in inputs:
-        script_sig = Script.p2pkh_unlocking_script(signature, pub_key)
-        signed_inputs.append(Input(inp.output, inp.tx_hash, script_sig))
-    return Transaction(signed_inputs, outputs)
+    pass
