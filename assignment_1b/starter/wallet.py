f9bd41703a70e5a338c7856934bfb8ece6c4b9f9
Jacob Gutsin <65308396+Gutsinj@users.noreply.github.com>
2026-02-24 17:16:52 -0500

    Implemented wallet.py

--- assignment_1b/starter/wallet.py
+++ assignment_1b/starter/wallet.py
@@ -34,4 +34,58 @@ def build_transaction(inputs: List[Input], outputs: List[Output], signing_key: S
     """
     # TODO: Implement build_transaction
     # Hint: Use Script.p2pkh_unlocking_script(signature, pub_key) for scriptSig
-    pass
+
+    # Run non-empty checks
+    if len(inputs) == 0 or len(outputs) == 0:
+        return None
+    
+    # Get pk and its hash
+    pub_key_bytes = signing_key.verify_key.encode()
+    pub_key = pub_key_bytes.hex()
+    pub_key_hash = sha256_hash(pub_key_bytes).hex()
+
+    # Validate inputs for no dupes, all are spendable, and compute total input value
+    seen_txids = set()
+    total_input_value = 0
+
+    for i in inputs:
+        if i.tx_hash in seen_txids:
+            return None
+        seen_txids.add(i.tx_hash)
+
+        output = i.output
+        script_pub_key = output.script_pubkey
+
+        elements = script_pub_key.elements
+        expected_hash = elements[2]
+        if expected_hash != pub_key_hash:
+            return None
+        
+        total_input_value += output.value
+    
+    # Check that input values equal output values
+    total_output_value = sum(o.value for o in outputs)
+    if total_input_value != total_output_value:
+        return None
+    
+    # Create unsigned transaction with empty scriptSigs
+    unsigned_inputs = []
+    for i in inputs:
+        unsigned_inputs.append(Input(i.output, i.tx_hash, Script([])))
+    tx = Transaction(unsigned_inputs, outputs)
+
+    # Sign the transaction data
+    tx_data = tx.bytes_to_sign()
+    tx_data_bytes = bytes.fromhex(tx_data)
+
+    signature_bytes = signing_key.sign(tx_data_bytes).signature
+    signature = signature_bytes.hex()
+
+    # Create scriptSig for each input and attach it
+    for i in tx.inputs:
+        i.script_sig = Script.p2pkh_unlocking_script(signature, pub_key)
+
+    # Recompute hash after adding each input's script_sig
+    tx.tx_hash = tx.get_hash()
+
+    return tx
